636ff8bcf836f93c754fc3936f5bf00bf8a59dc7
jmpqc <3138335779@qq.com>
2019-06-27 15:15:01 +0800

    MiniMap上创建了一个PointClickOnMap物体，该物体会根据鼠标在地图上的点击而改变位置

--- Assets/Scripte/UI/MiniMapNavigate.cs
+++ Assets/Scripte/UI/MiniMapNavigate.cs
@@ -5,10 +5,18 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class MiniMapNavigate : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler{
+    Transform pointOnMap; //鼠标在MiniMap上点击后，PointClickOnMap物体要移动到鼠标点击的位置，所以point相当于是鼠标点击的位置
+    float mapRectX;  //MiniMap在X方向上的尺寸
+    float mapRectY;  //MiniMap在Y方向上的尺寸
     // Use this for initialization
     void Start () {
+        pointOnMap = GameObject.Find("PointClickOnMap").transform;
+        //地图尺寸
+        mapRectX = GetComponent<RectTransform>().sizeDelta.x;
+        mapRectY = GetComponent<RectTransform>().sizeDelta.y;
 
-     }
+
+    }
 
     // Update is called once per frame
     void Update () {
@@ -17,7 +25,7 @@ public class MiniMapNavigate : MonoBehaviour, IPointerDownHandler, IPointerEnter
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        
+        pointOnMap.position = eventData.position; //将PointClickOnMap物体移动到鼠标点击的位置
     }
 
     public void OnPointerEnter(PointerEventData eventData)
